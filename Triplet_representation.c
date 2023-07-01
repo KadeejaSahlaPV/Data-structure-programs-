2316f257a1fe9faee18348e7a2ed708355c77ec1
KADEEJA SAHLA <99816026+KadeejaSahlaPV@users.noreply.github.com>
2023-07-01 11:56:17 +0530

    Triplet_representation of sparse matrix

---
+++ Triplet_representation.c
@@ -0,0 +1,29 @@
+#include<stdio.h>   
+int main() 
+{ 
+    int S[10][10],m,n,i,j,k=0,size=0;
+    printf("Enter number of rows in the matrix : ");
+    scanf("%d",&m);
+    printf("Enter number of columns in the matrix : ");
+    scanf("%d",&n);
+    printf("Enter elements in the sparse matrix : \n");
+    for (i = 0; i < m; i++) 
+        for (j = 0; j < n; j++) 
+            scanf("%d",&S[i][j]);
+    printf("The matrix is \n");
+    for (i = 0; i < m; i++) {
+        for (j = 0; j < n; j++) {
+            printf("  %d ",S[i][j]);
+        }
+        printf("\n");
+    }
+     printf("Triplet representation of the matrix is: \n");
+     for (i = 0; i < m; i++) {
+        for (j = 0; j < n; j++) 
+        {
+            if(S[i][j]!=0)
+            printf("\n %d  %d  %d",i,j,S[i][j]);
+        }
+        printf("\n");
+        }
+}
